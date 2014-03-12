962012d8485ded4398933250dcefe7d21570ee50
atscott <atscott01@gmail.com>
2014-03-12 13:24:02 -0500

    Initial commit

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+import java.util.List;
+
+/**
+ * User: atscott
+ * Date: 3/12/14
+ * Time: 1:18 PM
+ */
+public class InsertionSort implements NumberSorter
+{
+  @Override
+  public Integer[] Sort(Integer[] listOfNumbers)
+  {
+    for (int j = 2; j < listOfNumbers.length; j++)
+    {
+      int key = listOfNumbers[j];
+      int i = j - 1;
+      while (i > 0 && listOfNumbers[i] > key)
+      {
+        listOfNumbers[i + 1] = listOfNumbers[i];
+        i = i - 1;
+      }
+      listOfNumbers[i + 1] = key;
+    }
+    return null;
+  }
+}
---
+++ src/Sorting/NumberSorter.java
@@ -0,0 +1,13 @@
+package Sorting;
+
+import java.util.List;
+
+/**
+ * User: atscott
+ * Date: 3/12/14
+ * Time: 1:16 PM
+ */
+public interface NumberSorter
+{
+  public Integer[] Sort(Integer[] listOfNumbers);
+}
